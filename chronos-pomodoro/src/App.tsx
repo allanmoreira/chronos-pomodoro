45279522c537c628036d1681d38494d41e0c3c75
Allan Moreira <allanfelipe.moreira@gmail.com>
2025-12-30 19:58:39 -0300

    Container

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,10 +1,16 @@
 import './styles/theme.css'
 import './styles/global.css'
-import {Heading} from "./components/Heading.tsx";
+import {Heading} from "./components/heading/Heading.tsx";
+import {Container} from "./components/container/Container.tsx";
 import {TimerIcon} from "lucide-react";
 
 export function App() {
     return <>
+
+        <Container>
+            <section>LOGO</section>
+        </Container>
+
         <Heading>
             Hello, world!
             <button>
