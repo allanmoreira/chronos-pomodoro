d25b1dd623346daa851c0c7cb74bc7d4e300055e
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-01-04 16:11:17 -0300

    Textos menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,18 +2,33 @@ import styles from './styles.module.css'
 import {HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from "lucide-react";
 
 export function Menu() {
-    return(
+    const descricaoHome = "Ir para a home";
+    const descricaoHistorico = "Ver histórico";
+    const descricaoConfig = "Configurações";
+    const descricaoTema = "Alterar tema";
         <nav className={styles.menu}>
-            <a href="#" className={styles.menuLink}>
+            <a href="#"
+               className={styles.menuLink}
+               aria-label={descricaoHome}
+               title={descricaoHome}>
                 <HouseIcon/>
             </a>
-            <a href="#" className={styles.menuLink}>
+            <a href="#"
+               className={styles.menuLink}
+               aria-label={descricaoHistorico}
+               title={descricaoHistorico}>
                 <HistoryIcon/>
             </a>
-            <a href="#" className={styles.menuLink}>
+            <a href="#"
+               className={styles.menuLink}
+               aria-label={descricaoConfig}
+               title={descricaoConfig}>
                 <SettingsIcon/>
             </a>
             <a href="#" className={styles.menuLink}>
+               className={styles.menuLink}
+               aria-label={descricaoTema}
+               title={descricaoTema}
                 <SunIcon/>
             </a>
         </nav>)
