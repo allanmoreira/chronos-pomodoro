295ff0c92684ec9da28d23db2a089b738e4ea5df
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-01-08 00:14:43 -0300

    Texto

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import {HeartIcon} from "lucide-react";
 export function Footer() {
     return (
         <footer className={styles.footer}>
-            <a href=''>Entenda como funciona a ténica pomodoro</a>
+            <a href=''>Entenda como funciona a técnica pomodoro</a>
             <a href=''>Chronos pomodoro &copy; {new Date().getFullYear()} - Feito com <HeartIcon/></a>
         </footer>
     )
